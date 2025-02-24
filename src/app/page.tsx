e1d625bd35c4fe829b92daf735eb5c2bf3975d72
sergiorbotelho <sergiorbotelho@gmail.com>
2025-02-23 22:19:12 -0300

    feat: add testimonials component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
 import { Services } from "./_components/services";
+import { Testimonials } from "./_components/testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Testimonials />
     </main>
   );
 }
